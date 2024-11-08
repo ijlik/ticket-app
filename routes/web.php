bccc430c63a506c1040fe418e74fae6f8c288809
Nama Kamu <email@example.com>
2024-11-08 09:21:50 +0700

    perubahan authentication form login & register

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -24,3 +26,13 @@ Route::get('/dashboard', function () {
 Route::get('/events', function () {
     return view('events');
 });
+
+Route::get('/authentication', function () {
+    return view('authentication');
+});
+
+Route::get('/authentication', [AuthController::class, 'showLoginForm'])->name('authentication');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+
+Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
+Route::post('/register', [AuthController::class, 'register']);
