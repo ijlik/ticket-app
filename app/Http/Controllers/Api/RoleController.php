ec177f0fc0aacefde9d5790457e5e7b4250d3df4
ijlik <ijlik.k5s@gmail.com>
2024-11-13 10:47:46 +0700

    buat api create role

---
+++ app/Http/Controllers/Api/RoleController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreRoleRequest;
+use App\Http\Resources\RoleResource;
+use App\Models\Role;
+use Illuminate\Http\Request;
+
+class RoleController extends Controller
+{
+    public function store(StoreRoleRequest $request)
+    {
+        $newRole = Role::create(['name' => $request['role']]);
+
+        return RoleResource::make($newRole);
+    }
+}
